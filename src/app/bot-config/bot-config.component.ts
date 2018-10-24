4df83d7b4ebdd9c90df1f1458ef27e5b1f8fbb2c
sukeshrs <sukesrs@gmail.com>
2018-10-23 17:18:12 -0700

    changes for the main menu

--- src/app/bot-config/bot-config.component.ts
+++ src/app/bot-config/bot-config.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { menu } from '../constants/menu.constants';
 
 @Component({
   selector: 'bot-config',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BotConfigComponent implements OnInit {
 
+  public botConfigMenu : Array<any>;
+  public step : string;
+
   constructor() { }
 
   ngOnInit() {
+    console.log("The whole JSON in bot config");
+    console.log (menu);
+    this.botConfigMenu = [];
+
+    //Construct an array of menu items for the bot config object
+    Object.keys(menu.CREATE_NEW_BOT).forEach(key =>{
+      this.botConfigMenu.push(menu.CREATE_NEW_BOT[key]);
+    });
   }
 
 }
--- src/app/bot-dashboard/bot-dashboard.component.ts
+++ src/app/bot-dashboard/bot-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { menu } from '../constants/menu.constants';
 
 @Component({
   selector: 'bot-dashboard',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BotDashboardComponent implements OnInit {
 
+  public botDashboardMenu : Array<any>;
+
   constructor() { }
 
   ngOnInit() {
+    this.botDashboardMenu = [];
+    Object.keys(menu.YOUR_BOTS).forEach(key => {
+      this.botDashboardMenu.push(menu.YOUR_BOTS[key]);
+    })
   }
 
 }
