c4fc4e030c389c42200ddc8c46383bfa50744f61
victoriaagb <victoriaagb93@gmail.com>
2019-02-01 23:39:11 -0500

    fixed issues for creating a new bot config

--- src/app/shared/model/bot-config-repository.model.ts
+++ src/app/shared/model/bot-config-repository.model.ts
@@ -1,10 +1,11 @@
 import { BotConfig } from './bot-config.model';
 import { BotStatus } from './bot-status.enum';
+import { BotStepConfig } from './bot-step-config.enum';
 
 export interface BotConfigRepository {
   id?: string;
   botId: string;
-  stepConfig: string;
+  stepConfig: BotStepConfig;
   value: BotConfig;
   status: BotStatus;
   update_ts?: string;
