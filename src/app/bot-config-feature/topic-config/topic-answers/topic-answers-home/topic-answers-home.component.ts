3da1c5c73d4f615b87c93dbc85631edd954e716d
victoriaagb <victoriaagb93@gmail.com>
2019-11-24 19:21:01 -0500

    added questions to topic home response

--- src/app/bot-config-feature/topic-config/topic-answers/topic-answers-home/topic-answers-home.component.ts
+++ src/app/bot-config-feature/topic-config/topic-answers/topic-answers-home/topic-answers-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Response, TopicResponseType } from '../../../../shared/model/topic/response.model';
 
 @Component({
@@ -7,9 +7,15 @@ import { Response, TopicResponseType } from '../../../../shared/model/topic/resp
   styleUrls: ['./topic-answers-home.component.scss']
 })
 export class TopicAnswersHomeComponent implements OnInit {
+
   TopicResponseType = TopicResponseType;
   answer: Response;
+  public isCollapsed = false;
+
   @Output() addResponseEvent = new EventEmitter<Response>();
+  @Output() gotoQuestionEvent = new EventEmitter();
+  @Input() questions: string[];
+
   constructor() { }
 
   ngOnInit() {
@@ -20,6 +26,10 @@ export class TopicAnswersHomeComponent implements OnInit {
     this.answer.response_type = responseType;
     this.addResponseEvent.emit(this.answer);
     this.answer = <Response>{};
+  }
+
+  gotoQuestions() {
+    this.gotoQuestionEvent.emit();
   }
 
 }
